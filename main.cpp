2c1996f4d8c876f92ca1923ae50014b294ad4352
Ivan Nechta <tal200@lenta.ru>
2023-11-10 18:34:29 +0700

    v.2.1: Terminal version

--- main.cpp
+++ main.cpp
@@ -5,16 +5,25 @@
 #include "NdimArray.h"
 
 
-#define FILENAME "e:\\temp\\Entropy2\\commands_from_exe.txt"
+//#define FILENAME "e:\\temp\\Entropy2\\commands_from_exe.txt"
 
 using namespace std;
-int main()
+void Usage(char *s){
+    printf("Usage: %s <debug.file> <Max_memory_len>\n", s);
+    printf("Example:\n%s a.asm 12\n", s);
+}
+int main(int argc, char **argv)
 {
+    if (argc!=3){
+        Usage(argv[0]);
+        return -1;
+    }
+
     DataBase *DB;
-    for (int i=1;i<=12;i++){
+    for (int i=1;i<=20;i++){
         DB=new DataBase();
-        DB->ReadChains(i, (char *)FILENAME);
-        printf("Memory=%d, %.3f\n",i,DB->CalcEntropy());
+        DB->ReadChains(i, argv[1]);
+        printf("Memory=%d Entropy=%.3f\n",i,DB->CalcEntropy());
         delete DB;
     }
 
